9db9ece652ca1be9e859a1b88e95ab9ffdedb484
Benjamin Staugaard <bemmamin@live.dk>
2019-01-12 22:52:14 +0100

    Better stars

--- authorstats.js
+++ authorstats.js
@@ -6,6 +6,7 @@ table.innerHTML = statsTable;
 table.querySelector("#author").innerHTML = isProfilePage ? document.querySelector(".secondaryContent h1").textContent : element.querySelector(".titleBar").textContent.split(" ")[2];
 
 let isLoaded = false;
+let firstLoad = true;
 if (isProfilePage) {
     table.style.marginLeft = "20px";
     document.querySelectorAll(".Tabs li").forEach(value => {
@@ -16,6 +17,12 @@ if (isProfilePage) {
                 isLoaded = true;
                 element.prepend(table);
                 loadStats(isProfilePage);
+                if (firstLoad) {
+                    firstLoad = false;
+                    window.setTimeout(function () {
+                        replaceStars();
+                    }, 1000);
+                }
             } else {
                 if (!isLoaded) return;
                 isLoaded = false;
@@ -25,11 +32,36 @@ if (isProfilePage) {
         })
     })
 } else {
+    replaceStars();
     if (!window.location.href.includes("page") || window.location.href.includes("page=1")) {
         element.prepend(table);
         window.setTimeout(function () {
             document.querySelector(".titleBar").remove();
         }, 20);
         loadStats(isProfilePage);
+    }
+}
+
+function replaceStars() {
+    let resources = document.querySelectorAll(".resourceListItem");
+    for (let i = 0; i < resources.length; i++) {
+        let resource = resources[i];
+        let newStarsSpan = document.createElement("span");
+        let ratingString = resource.querySelector(".ratings").getAttribute("title");
+        let fullStars = parseInt(ratingString.charAt(0));
+        let commaStarSet = false;
+        let j = 1;
+        resource.querySelector(".ratings").childNodes.forEach(function (span) {
+            if (span.className === undefined) return;
+            let star = document.createElement("img");
+            star.src = valImg[j <= fullStars ? 1.000 : 0.000];
+            if (j > fullStars && !commaStarSet) {
+                commaStarSet = true;
+                star.src = getImage(parseFloat(ratingString.includes(".") ? ratingString.replace(ratingString.charAt(0), "0") : 0));
+            }
+            newStarsSpan.appendChild(star);
+            j++;
+        });
+        resource.querySelector(".ratings").replaceWith(newStarsSpan);
     }
 }
